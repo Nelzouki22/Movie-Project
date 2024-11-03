73fa0a08b0659bb36300844a7398883153a96bf0
Nelzouki22 <elzoukigroup2018@gmail.com>
2024-11-02 21:59:18 -0400

    Initial Commit

---
+++ Movie Project/Movie_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ProjectApp.swift
+//  Movie Project
+//
+//  Created by NADIR ELZOUKI on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie ProjectTests/Movie_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ProjectTests.swift
+//  Movie ProjectTests
+//
+//  Created by NADIR ELZOUKI on 11/2/24.
+//
+
+import Testing
+@testable import Movie_Project
+
+struct Movie_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
